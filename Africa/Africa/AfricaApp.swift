a7f00271c0dce68bebace80c5bdafc046c7e9fd9
jesusfortuna <fortuna.codes@gmail.com>
2023-06-05 17:02:11 +0200

    Initial set up

---
+++ Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Jesús  Fortuna on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
