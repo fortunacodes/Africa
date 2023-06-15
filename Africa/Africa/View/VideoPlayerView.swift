725a1f68316800ec57fa6b50cdf61cfa2f36eb1a
jesusfortuna <fortuna.codes@gmail.com>
2023-06-15 22:49:02 +0200

    Video Player has been implemented

---
+++ Africa/Africa/View/VideoPlayerView.swift
@@ -0,0 +1,47 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by Jesús  Fortuna on 15/6/23.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    //MARK: - PROPERTY
+    var videoSelected: String
+    var videoTitle: String
+    
+    //MARK: - BODY
+    var body: some View {
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")){
+                //Text(videoTitle)
+            }
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8)
+                ,alignment: .topLeading
+            )
+        }//: VSTACK
+        .accentColor(.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationTitle(videoTitle)
+    }
+}
+
+//MARK: - PREVIEW
+struct VideoPlayerView_Previews: PreviewProvider {
+    
+    
+    static var previews: some View {
+        NavigationView {
+            VideoPlayerView(videoSelected: "cheetah", videoTitle: "Cheetah")
+        }
+    }
+}
